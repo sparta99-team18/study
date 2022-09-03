dbe85c0bb1caa79f040ead3930f420ed995d3270
Doosan <57098232+BaekDoosan-maker@users.noreply.github.com>
2022-09-03 11:22:50 +0900

    study first commit , hanghae

---
+++ hanghae/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, session, redirect
+from functools import wraps
+import pymongo
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+
+# Database
+client = MongoClient('mongodb+srv://test:sparta@cluster0.9cihgwo.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
+
+# Routes
+from user import routes
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+  return render_template('dashboard.html')
