d144f45cdb7549e61b45558f36d423726e9648b6
Doosan <57098232+BaekDoosan-maker@users.noreply.github.com>
2022-09-15 14:20:02 +0900

    bucket 내용 삭제 커밋

---
+++ static/js/scripts.js
@@ -0,0 +1,47 @@
+$("form[name=signup_form]").submit(function(e) {
+
+  let $form = $(this);
+  let $error = $form.find(".error");
+  let data = $form.serialize();
+
+  $.ajax({
+    url: "/user/signup",
+    type: "POST",
+    data: data,
+    dataType: "json",
+    success: function(resp) {
+      window.location.href = "/dashboard/";
+    },
+    error: function(resp) {
+      $error.text(resp.responseJSON.error).removeClass("error--hidden");
+    }
+  });
+
+  e.preventDefault();
+});
+
+$("form[name=login_form]").submit(function(e) {
+
+  let $form = $(this);
+  let $error = $form.find(".error");
+  let data = $form.serialize();
+
+  $.ajax({
+    url: "/user/login",
+    type: "POST",
+    data: data,
+    dataType: "json",
+    success: function(resp) {
+      window.location.href = "/dashboard/";
+    },
+    error: function(resp) {
+      $error.text(resp.responseJSON.error).removeClass("error--hidden");
+    }
+  });
+
+  e.preventDefault();
+});
+
+
+
+
